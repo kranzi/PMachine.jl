37fb25c8794c443bd53f3ba3fd783ba21603d96f
Kranzi <kranzi.kranzhuber@gmail.com>
2018-04-22 21:15:50 +0200

    import PMachine for debugging
    use import instead of using

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,21 +1,21 @@
-using PMachine
+import PMachine
 using Base.Test
 
-r1 = Register{ UInt8 }( 1 )
+r1 = PMachine.Register{ UInt8 }( 1 )
 
-r2 = Register{ UInt8 }()
+r2 = PMachine.Register{ UInt8 }()
 
 @test ( typeof( r1 ) == typeof( r2 ) )
 
 @test get( r2 ) == 0x00
 
-set( r2, UInt8( 1 ) )
+PMachine.set( r2, UInt8( 1 ) )
 
 @test get( r1 ) == get( r2 )
 
-m = Memory{ UInt8, UInt8 }( 10 )
+m = PMachine.Memory{ UInt8, UInt8 }( 10 )
 
-@test getAddr( m ) == 0
+@test PMachine.getAddr( m ) == 0
 
 @test PMachine.read( m ) == 0
 
@@ -23,13 +23,13 @@ PMachine.write( m, UInt8(99) )
 
 @test PMachine.read( m ) == 99
 
-setAddr( m, UInt8( 2 ) )
+PMachine.setAddr( m, UInt8( 2 ) )
 PMachine.reset( m )
 
 @test PMachine.read( m ) == 0
-@test getAddr( m ) == 0
+@test PMachine.getAddr( m ) == 0
 
-ma = Machine()
+ma = PMachine.Machine()
 
-@test codesize( ma ) == 100
-@test storesize( ma ) == 100
+@test PMachine.codesize( ma ) == 100
+@test PMachine.storesize( ma ) == 100
